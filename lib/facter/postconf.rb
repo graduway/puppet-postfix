b6bdb70c344a301c024fdfc79c77a1b951da1ab3
Shachar Shapira <shapi78@gmail.com>
2018-11-09 12:32:21 +0200

    Adding windows condition

--- lib/facter/postconf.rb
+++ lib/facter/postconf.rb
@@ -1,21 +1,23 @@
-Facter.add(:postfix) do
-  setcode do
-    postconf = Facter::Util::Resolution.which('postconf')
-    if postconf.nil?
-      next nil
-    end
+if Facter.value('operatingsystem') != 'windows'
+	Facter.add(:postfix) do
+		setcode do
+			postconf = Facter::Util::Resolution.which('postconf')
+			if postconf.nil?
+				next nil
+			end
 
-    configs = {
-      mail_version: :version,
-      config_directory: :default_config_directory,
-      data_directory: :default_data_directory,
-    }
+			configs = {
+				mail_version: :version,
+				config_directory: :default_config_directory,
+				data_directory: :default_data_directory,
+			}
 
-    facts = {}
-    Facter::Core::Execution.execute("#{postconf} -d -x #{configs.keys.join(' ')}").each_line do |line|
-      parameter, value = line.chomp.split(%r{ *= *}, 2)
-      facts[configs[parameter.to_sym]] = value
-    end
-    facts
-  end
+			facts = {}
+			Facter::Core::Execution.execute("#{postconf} -d -x #{configs.keys.join(' ')}").each_line do |line|
+				parameter, value = line.chomp.split(%r{ *= *}, 2)
+				facts[configs[parameter.to_sym]] = value
+			end
+			facts
+		end
+	end
 end
